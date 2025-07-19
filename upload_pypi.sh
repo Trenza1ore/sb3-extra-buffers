981ba0425a11b2bc96b4448ea35caa18aa6a4a15
Hugo <pondypondo@outlook.com>
2025-07-19 12:14:43 +0100

    fix: remove unwanted files

--- upload_pypi.sh
+++
@@ -1,6 +0,0 @@
-pip uninstall sb3_extra_buffers
-python setup.py install
-python setup.py sdist bdist_wheel
-twine upload --skip-existing dist/*.whl
-twine upload --skip-existing dist/*.tar.gz
-REM As of 2023, egg files are no longer accepted for PyPI uploads
